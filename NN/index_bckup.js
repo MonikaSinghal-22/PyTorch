7c5220ed4c926115a54f1c1ce7199def995829da
Monika <39440249+MonikaSinghal-22@users.noreply.github.com>
2023-10-16 19:11:39 -0500

    Update and rename index.js to index_bckup.js

--- NN/index_bckup.js
+++ NN/index_bckup.js
@@ -20,7 +20,7 @@ async function runExample() {
     let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
 
     let session = new onnx.InferenceSession();
-
+#await session.loadModel("./DLnet_WineData.onnx");
     await session.loadModel("DLnet_WineData.onnx");
     let outputMap = await session.run([tensorX]);
     let outputData = outputMap.get('output1');
